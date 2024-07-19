25f9e0b366e26c7730f39c12f485bde64c15aa73
riya-0105 <riyasushma2019@gmail.com>
2024-07-20 00:11:44 +0530

    path loaded

--- backend/src/globe-data/globe-data.service.ts
+++ backend/src/globe-data/globe-data.service.ts
@@ -5,8 +5,20 @@ import * as path from 'path';
 @Injectable()
 export class GlobeDataService {
     getData() {
-        const filePath = path.resolve(__dirname, '..',  '..', '..', 'scripts', 'data.json');
-        const data = fs.readFileSync(filePath, 'utf-8');
-        return JSON.parse(data);
+        try {
+            console.log("path dir: ", path.resolve(__dirname, '..',  '..', '..', 'scripts', 'data.json'));
+            console.log("path env: ", process.env.DATA_FILE_PATH);
+            
+            const filePath = process.env.DATA_FILE_PATH || path.resolve(__dirname, '..',  '..', '..', 'scripts', 'data.json');
+            if (!fs.existsSync(filePath)) {
+                console.error('File not found:', filePath);
+                return null;
+            }
+            const data = fs.readFileSync(filePath, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.error('Error reading or parsing file:', error);
+            return null;
+        }
     }
 }
